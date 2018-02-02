d6bb182c5fc650481442ab7aa018e05d3e336782
lambq <415113157@qq.com>
2018-02-02 20:25:06 +0800

    lambq 1

---
+++ src/GeetestProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lamb\GeeTest;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeetestProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $this->publishes([
+            __DIR__.'/config/lamb.php' => config_path('lamb.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/config/lamb.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * config file for lamb
+ * User: lamb
+ * Date: 2018/2/2
+ * Time: 下午8:12
+ */
+return [
+    'baidu_push_zz_site'    => '',
+    'baidu_push_zz_token'   => '',
+    'zz_api'                => '',
+    'zz_host'               => '',
+];
