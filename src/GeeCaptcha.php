cc72fac86583e2ce5a29f4349ce6c173212124cf
lambq <415113157@qq.com>
2018-02-06 10:59:33 +0800

    lambq 1

--- src/GeeCaptcha.php
+++ src/GeeCaptcha.php
@@ -21,48 +21,45 @@ class GeeCaptcha extends GeetestLib
     }
 
     /**
-     * @param  \Illuminate\Http\Request  $request
      * @return bool|mixed|string
      *
      * 判断验证是否成功
      */
-    public function success(Request $request)
+    public function success()
     {
         $data = [
-            "user_id" => $request->user()->id."_yun", # 网站用户id
+            "user_id" => request()->id()."_yun", # 网站用户id
             "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-            "ip_address" => $request->getClientIp() # 请在此处传输用户请求验证时所携带的IP
+            "ip_address" => request()->ip() # 请在此处传输用户请求验证时所携带的IP
         ];
-        $result = $this->success_validate($request->get('geetest_challenge'), $request->get('geetest_validate'), $request->get('geetest_seccode'),$data);
+        $result = $this->success_validate(request()->input('geetest_challenge'), request()->input('geetest_validate'), request()->input('geetest_seccode'),$data);
 
         return $result;
     }
 
     /**
-     * @param  \Illuminate\Http\Request  $request
      * @return int
      *
      * GT 服务器是否有回应
      */
-    public function hasAnswer(Request $request)
+    public function hasAnswer()
     {
-        return $this->fail_validate($request->get('geetest_challenge'), $request->get('geetest_validate'), $request->get('geetest_seccode'));
+        return $this->fail_validate(request()->input('geetest_challenge'), request()->input('geetest_validate'), request()->input('geetest_seccode'));
     }
 
     /**
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return mixed
      *
      * 判断GT 服务器是否正常
      */
-    public function GTServerIsNormal(Request $request)
+    public function GTServerIsNormal()
     {
         session_start();
         $data = [
-            "user_id" => $request->user()->id."_yun", # 网站用户id
+            "user_id" => request()->id()."_yun", # 网站用户id
             "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-            "ip_address" => $request->getClientIp() # 请在此处传输用户请求验证时所携带的IP
+            "ip_address" => request()->ip() # 请在此处传输用户请求验证时所携带的IP
         ];
         $status = $this->pre_process($data,1);
         $_SESSION['gtserver'] = $status;
